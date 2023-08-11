3040bcf643a00b502e1f31d9c77b5d41d3a049ee
PHUTNPH17442 <phutnph17442@fpt.edu.vn>
2023-08-12 03:12:00 +0700

    fix myFace

--- controllers/myStatusController.js
+++ controllers/myStatusController.js
@@ -3,8 +3,7 @@ const Blog = require('../models/blog')
 const myStatus = async(req,res)=>{
     const userId = req.session.userId;
     const userData = req.session.userData;
-    populate('author','username avatar description').sort({createAt :-1}).exec()
-    const blog = await Blog.find({author : userId}).
+    const blog = await Blog.find({author : userId}). populate('author','username avatar description').sort({createAt :-1}).exec()
     blog.forEach(blog =>{
         blog.formattedDate = blog.createAt.toLocaleDateString();
       })
